73c28c1b2e7fc92932c9f25c85b3729ae98b1797
Kim Hyeong Kyu <hyongkui@nate.com>
2022-12-01 17:26:50 +0900

    edit

--- api_station.py
+++ api_station.py
@@ -11,15 +11,14 @@ url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?serviceKey={k
 
 content = requests.get(url).content # GET요청
 dict = xmltodict.parse(content) # XML을 dictionary로 파싱
+data = dict['ServiceResult']['msgBody']['itemList']
 
-
-jsonString = json.dumps(dict['ServiceResult']['msgBody']['itemList'], ensure_ascii=False) # dict을 json으로 변환
-jsonObj = json.loads(jsonString)
 
 def getStationAll():
     station_list = []
-    for i in range(len(jsonObj)) :
-        bus_station = jsonObj[i]['stationNm']
+    for i in range(len(data)) :
+        bus_station = data[i]['stationNm']
         station_list.append(bus_station)
     return station_list
 
+print(getStationAll())

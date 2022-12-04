0a03d1322dc1255329ca6340f731c844d641d752
Kim Hyeong Kyu <hyongkui@nate.com>
2022-12-04 15:11:32 +0900

    db

--- add_db.py
+++ add_db.py
@@ -18,25 +18,32 @@ station_list = getStationAll()
 
 
 
-def addbusdata():
-    for bus_data in bus_list:
-        sql = f"INSERT INTO bus (bus_name, bus_id) VALUES ('{bus_data['bus_name']}', {bus_data['bus_id']})"
-        curs.execute(sql)
-        conn.commit()
 
-    print("DB 저장 완료")
-    conn.close()
+# def addbusdata():
+#     for bus_data in bus_list:
+#         sql = f"INSERT INTO bus (bus_name, bus_id) VALUES ('{bus_data['bus_name']}', {bus_data['bus_id']})"
+#         curs.execute(sql)
+#         conn.commit()
+#
+#     print("DB 저장 완료")
+#     conn.close()
+
 
 
 def addstationdata():
     for station_data in station_list:
-        sql = f"INSERT INTO station (station_name, bus_id) VALUES ('{station_data}', 100100412)"
+        sql = f"INSERT INTO station (station, stationNm, stationNo, gpsX, gpsY)" \
+              f" VALUES ({station_data['station']}, " \
+              f"'{station_data['stationNm']}', " \
+              f"'{station_data['stationNo']}', " \
+              f"{station_data['gpsX']}, " \
+              f"{station_data['gpsY']})"
         curs.execute(sql)
         conn.commit()
     print("정류소 저장 완료")
     conn.close()
 
+addstationdata()
 
-addbusdata()
 
 

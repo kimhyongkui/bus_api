ec147ac5d9ccda17a904eef79d83b7d81382445a
Kim Hyeong Kyu <hyongkui@nate.com>
2022-12-04 15:12:55 +0900

    edit

--- api_bus.py
+++ api_bus.py
@@ -17,14 +17,13 @@ data = dict['ServiceResult']['msgBody']['itemList']
 
 def getBusAll():
     bus_list = []
-    for i in range(len(data)):
+    for bus in range(len(data)):
         bus_dict = {}
-        bus_name = data[i]["busRouteNm"]
-        bus_Id = data[i]["busRouteId"]
+        bus_name = data[bus]["busRouteNm"]
+        bus_Id = data[bus]["busRouteId"]
         bus_dict["bus_name"] = bus_name
         bus_dict["bus_id"] = bus_Id
         bus_list.append(bus_dict)
-        print(bus_dict)
     return bus_list
 
 getBusAll()

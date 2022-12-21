b2daf0ead1abb8f2b10b869e3f346dd1ccf4c171
Kim Hyeong Kyu <hyongkui@nate.com>
2022-12-21 23:49:50 +0900

    edit and add arrive info

--- add_db.py
+++ add_db.py
@@ -1,6 +1,7 @@
 from api_bus import get_busall
 from api_station import get_stationall, get_station
 from api_busvehid import get_businfo, get_businfoall
+from api_arrinfo import get_arrive_info, get_arrive_infoall
 from dotenv import load_dotenv
 import pymysql
 import os
@@ -35,8 +36,8 @@ def add_busdata():
 #-----------------------------------------------------------------------
 
 # 특정 노선의 정류소 DB저장
-def add_station():
-    st_list = get_station()
+def add_station(busid):
+    st_list = get_station(busid)
     for station_data in st_list:
         sql = f"INSERT INTO station (bus_id, station, stationNm, stationNo, gpsX, gpsY)" \
               f" VALUES ({station_data['bus_id']}, " \
@@ -69,8 +70,8 @@ def add_stationall():
 
 #-----------------------------------------------------------------------
 # 특정 노선의 차량 DB저장
-def add_vehdata():
-    veh_list = get_businfo()
+def add_vehdata(busid):
+    veh_list = get_businfo(busid)
     for vehicle_data in veh_list:
         sql = f"INSERT INTO vehicle (bus_id, vehId, plainNo)" \
               f"VALUES ({vehicle_data['bus_id']}, " \
@@ -96,4 +97,36 @@ def add_vehalldata():
         conn.commit()
     print("차량 정보 저장 완료")
     conn.close()
+#-----------------------------------------------------------------------
+# 특정 경유노선의 전체정류소 DB 저장
+def add_arrive(busid):
+    arr_list = get_arrive_info(busid)
+    for arr_data in arr_list:
+        sql = f"INSERT INTO arrive (bus_id, rtNm, staOrd, stNm)" \
+              f" VALUES ({arr_data['bus_id']}, " \
+              f"'{arr_data['rtNm']}', " \
+              f"{arr_data['staOrd']}, " \
+              f"'{arr_data['stNm']}')"
 
+        curs.execute(sql)
+        conn.commit()
+    print("데이터 저장 완료")
+    conn.close()
+
+
+# 모든 노선의 정류소 DB저장
+def add_arriveall():
+    arrall_list = get_arrive_infoall()
+    for arr_data in arrall_list:
+        sql = f"INSERT INTO station (bus_id, rtNm, staOrd, stNm)" \
+              f" VALUES ({arr_data['bus_id']}, " \
+              f"'{arr_data['rtNm']}', " \
+              f"{arr_data['staOrd']}, " \
+              f"'{arr_data['stNm']}')"
+
+        curs.execute(sql)
+        conn.commit()
+    print("정류소 저장 완료")
+    conn.close()
+
+add_arriveall()

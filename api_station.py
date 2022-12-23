2e932b087c4605ba4e69b412e3427e6989f6117e
Kim Hyeong Kyu <hyongkui@nate.com>
2022-12-24 01:03:29 +0900

    add column

--- api_station.py
+++ api_station.py
@@ -1,90 +1,103 @@
 import requests, xmltodict
 from dotenv import load_dotenv
 import os
-from api_bus import get_bus_all
+from api_route import get_route_all
 
 
 load_dotenv()
 key = os.getenv('key')
 
-# 특정 노선의 정류소 데이터 얻기
+# 특정 노선의 경유 정류소 데이터 얻기
 
-def get_station(busid):
-    url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?serviceKey={key}&busRouteId={busid}"
+def get_station(route_id):
+    url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?serviceKey={key}&busRouteId={route_id}"
     content = requests.get(url).content
     dict = xmltodict.parse(content)
     data = dict['ServiceResult']['msgBody']['itemList']
-    station_list = []
-    for station in range(len(data)) :
-        station_dict = {}
-        station_id = data[station]['station']
-        station_name = data[station]['stationNm']
-        station_no = data[station]['stationNo']
-        station_gpsx = data[station]['gpsX']
-        station_gpsy = data[station]['gpsY']
+    stn_list = []
+    for station in range(len(data)):
+        stn_dict = {}
+        route_name = data[station]['busRouteNm'] # 노선명
+        route_abrv = data[station]['busRouteAbrv']  # 노선명
+        stn_id = data[station]['station'] #정류소 ID
+        stn_name = data[station]['stationNm'] #정류소 이름
+        stn_no = data[station]['arsId'] #정류소 고유번호
+        stn_dir = data[station]['direction'] #진행방향
+        stn_gpsx = data[station]['gpsX'] #정류소 좌표
+        stn_gpsy = data[station]['gpsY'] #정류소 좌표
 
-        station_dict['bus_id'] = busid
-        station_dict['station'] = station_id
-        station_dict['stationNm'] = station_name
-        station_dict['stationNo'] = station_no
-        station_dict['gpsX'] = station_gpsx
-        station_dict['gpsY'] = station_gpsy
-        station_list.append(station_dict)
+        stn_dict['routeId'] = route_id
+        stn_dict['busRouteNm'] = route_name
+        stn_dict['busRouteAbrv'] = route_abrv
+        stn_dict['station'] = stn_id
+        stn_dict['stationNm'] = stn_name
+        stn_dict['arsId'] = stn_no
+        stn_dict['direction'] = stn_dir
+        stn_dict['gpsX'] = stn_gpsx
+        stn_dict['gpsY'] = stn_gpsy
+        stn_list.append(stn_dict)
+    return stn_list
 
-    return station_list
 
 
 # 모든 정류소 데이터 얻기
 def get_station_all():
-    bus_list = get_bus_all()
-    station_list = []
-    for bus in bus_list:
-        busid = bus['bus_id']
-        url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?serviceKey={key}&busRouteId={busid}"
+    route_list = get_route_all()
+    stn_list = []
+    for route in route_list:
+        route_id = route['routeId']
+        url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?serviceKey={key}&busRouteId={route_id}"
         content = requests.get(url).content
         dict = xmltodict.parse(content)
         data = dict['ServiceResult']['msgBody']['itemList']
         for station in range(len(data)):
-            station_dict = {}
-            station_id = data[station]['station']
-            station_name = data[station]['stationNm']
-            station_no = data[station]['stationNo']
-            station_gpsx = data[station]['gpsX']
-            station_gpsy = data[station]['gpsY']
+            stn_dict = {}
+            route_name = data[station]['busRouteNm']  # 노선명
+            route_abrv = data[station]['busRouteAbrv']  # 노선명
+            stn_id = data[station]['station']  # 정류소 ID
+            stn_name = data[station]['stationNm']  # 정류소 이름
+            stn_no = data[station]['arsId']  # 정류소 고유번호
+            stn_dir = data[station]['direction']  # 진행방향
+            stn_gpsx = data[station]['gpsX']  # 정류소 좌표
+            stn_gpsy = data[station]['gpsY']  # 정류소 좌표
 
-            station_dict['bus_id'] = busid
-            station_dict['station'] = station_id
-            station_dict['stationNm'] = station_name
-            station_dict['stationNo'] = station_no
-            station_dict['gpsX'] = station_gpsx
-            station_dict['gpsY'] = station_gpsy
-            station_list.append(station_dict)
-    return station_list
+            stn_dict['routeId'] = route_id
+            stn_dict['busRouteNm'] = route_name
+            stn_dict['busRouteAbrv'] = route_abrv
+            stn_dict['station'] = stn_id
+            stn_dict['stationNm'] = stn_name
+            stn_dict['arsId'] = stn_no
+            stn_dict['direction'] = stn_dir
+            stn_dict['gpsX'] = stn_gpsx
+            stn_dict['gpsY'] = stn_gpsy
+            stn_list.append(stn_dict)
+
+    return stn_list
+
 
 # 특정 좌표 인근 정류소 데이터 얻기
-def get_station_list(tmx, tmy, radius):
+def get_stn_list(tmx, tmy, radius):
     url = f"http://ws.bus.go.kr/api/rest/stationinfo/getStationByPos?serviceKey={key}&tmX={tmx}&tmY={tmy}&radius={radius}"
     content = requests.get(url).content
     dict = xmltodict.parse(content)
     data = dict['ServiceResult']['msgBody']['itemList']
-    station_list = []
+    stn_list = []
     for station in range(len(data)):
-        station_dict = {}
-        station_id = data[station]['stationId']
-        station_name = data[station]['stationNm']
-        station_no = data[station]['arsId']
-        station_gpsx = data[station]['gpsX']
-        station_gpsy = data[station]['gpsY']
-        station_dist = data[station]['dist']
+        stn_dict = {}
+        stn_id = data[station]['stationId']
+        stn_name = data[station]['stationNm']
+        stn_no = data[station]['arsId']
+        stn_gpsx = data[station]['gpsX']
+        stn_gpsy = data[station]['gpsY']
+        stn_dist = data[station]['dist']
 
-        station_dict['stationId'] = station_id
-        station_dict['stationNm'] = station_name
-        station_dict['arsId'] = station_no
-        station_dict['gpsX'] = station_gpsx
-        station_dict['gpsY'] = station_gpsy
-        station_dict['dist'] = station_dist
-        station_list.append(station_dict)
-        print(station_dict)
-    return station_list
+        stn_dict['stationId'] = stn_id
+        stn_dict['stationNm'] = stn_name
+        stn_dict['arsId'] = stn_no
+        stn_dict['gpsX'] = stn_gpsx
+        stn_dict['gpsY'] = stn_gpsy
+        stn_dict['dist'] = stn_dist
+        stn_list.append(stn_dict)
+        print(stn_dict)
+    return stn_list
 
-get_station_list(126.9433, 37.48235, 150)

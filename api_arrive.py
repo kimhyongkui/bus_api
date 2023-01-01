a2ca677f02aa5fedbd4b24fbb61b13b8086eaacc
Kim Hyeong Kyu <hyongkui@nate.com>
2023-01-01 23:17:51 +0900

    Re start 2023

--- api_arrive.py
+++ api_arrive.py
@@ -112,4 +112,53 @@ def get_arrive_info(stnId, routeid, ord):
     return arrive_list
 
 
+# 특정 정류소에 도착 예정인 버스들
+def get_arrive_bus_info():
+    def get_arrive_info(stnId, routeid, ord):
+        url = f"http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRoute?" \
+              f"serviceKey={key}&stId={stnId}&busRouteId={routeid}&ord={ord}"
+        content = requests.get(url).content
+        xmldict = xmltodict.parse(content)
+        data = xmldict['ServiceResult']['msgBody']
+
+        arrive_list = []
+        arrive_rtId = routeid
+        arrive_ord = ord
+
+        if data is None:
+            print('데이터가 없습니다')
+
+        elif isinstance(data['itemList'], dict):
+            data_list = []
+            data_list.append(data['itemList'])
+
+            for arrive in range(len(data_list)):
+                arrive_dict = {}
+
+                arrive_dict['routeId'] = arrive_rtId
+                arrive_dict['routeNm'] = data_list[arrive]['rtNm']
+                arrive_dict['stnOrd'] = arrive_ord
+                arrive_dict['stnNm'] = data_list[arrive]['stNm']
+                arrive_dict['stnId'] = data_list[arrive]['stId']
+                arrive_dict['plainNo1'] = data_list[arrive]['plainNo1']
+                arrive_dict['arrmsg1'] = data_list[arrive]['arrmsg1']
+                arrive_dict['stnNm1'] = data_list[arrive]['stationNm1']
+                arrive_dict['plainNo2'] = data_list[arrive]['plainNo2']
+                arrive_dict['arrmsg2'] = data_list[arrive]['arrmsg2']
+                arrive_dict['stnNm2'] = data_list[arrive]['stationNm2']
+
+                arrive_list.append(arrive_dict)
+                print(f"노선ID : {arrive_rtId},"
+                      f" 노선이름 : {arrive_rtNm},"
+                      f" 노선순번 : {arrive_ord},"
+                      f" 정류소이름 : {arrive_stnNm},"
+                      f" 노선ID : {arrive_stnId},"
+                      f" 첫번째 도착예정버스 : {arrive_No1},"
+                      f" 현재 정류장 : {arrive_stnNm1},"
+                      f" 도착예정시간 : {arrive_msg1},"
+                      f" 두번째 도착예정버스 : {arrive_No2},"
+                      f" 도착예정시간 : {arrive_msg2},"
+                      f" 현재 정류장 : {arrive_stnNm2}")
+
+        return arrive_list
 

b7e907e55e5d8ff585957bca9211064b23462ca1
Kim Hyeong Kyu <hyongkui@nate.com>
2023-01-04 16:08:21 +0900

    get arrive bus info

--- api_arrive.py
+++ api_arrive.py
@@ -99,80 +99,77 @@ def get_arrive_info(stnId, routeid, ord):
 
             arrive_list.append(arrive_dict)
             print(f"노선ID : {arrive_rtId},"
-                  f" 노선이름 : {arrive_rtNm},"
+                  f" 노선이름 : {arrive_dict['routeNm']},"
                   f" 노선순번 : {arrive_ord},"
-                  f" 정류소이름 : {arrive_stnNm},"
-                  f" 노선ID : {arrive_stnId},"
-                  f" 첫번째 도착예정버스 : {arrive_No1},"
-                  f" 현재 정류장 : {arrive_stnNm1},"
-                  f" 도착예정시간 : {arrive_msg1},"
-                  f" 두번째 도착예정버스 : {arrive_No2},"
-                  f" 도착예정시간 : {arrive_msg2},"
-                  f" 현재 정류장 : {arrive_stnNm2}")
+                  f" 정류소이름 : {arrive_dict['stnNm']},"
+                  f" 정류소ID : {arrive_dict['stnId']},"
+                  f" 첫번째 도착예정버스 : {arrive_dict['plainNo1']},"
+                  f" 도착예정시간 : {arrive_dict['arrmsg1']},"
+                  f" 현재 정류장 : {arrive_dict['stnNm1']},"
+                  f" 두번째 도착예정버스 : {arrive_dict['plainNo2']},"
+                  f" 도착예정시간 : {arrive_dict['arrmsg2']},"
+                  f" 현재 정류장 : {arrive_dict['stnNm2']}")
 
     return arrive_list
 
 
 # 특정 정류소에 도착 예정인 버스들
-def get_arrive_bus_info():
-
-        return arrive_list
-
-def get_some_data(table, column, value):
+def get_arrive_bus_info(table, column1, value1, column2, value2):
     curs = conn.cursor()
-    sql = f"SELECT * FROM {table} WHERE {column}='{value}'"
+    sql = f"SELECT * FROM {table} WHERE {column1}='{value1}' AND {column2}={value2}"
     curs.execute(sql)
     result = curs.fetchall()
     for i in result:
-        print(i)
+        ddd = {}
+        ddd['stnId'] = i[4]
+        ddd['routeId'] = i[0]
+        ddd['ord'] = i[2]
 
-        # def get_arrive_info(stnId, routeid, ord):
-        #     url = f"http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRoute?" \
-        #           f"serviceKey={key}&stId={stnId}&busRouteId={routeid}&ord={ord}"
-        #     content = requests.get(url).content
-        #     xmldict = xmltodict.parse(content)
-        #     data = xmldict['ServiceResult']['msgBody']
-        #
-        #     arrive_list = []
-        #     arrive_rtId = routeid
-        #     arrive_ord = ord
-        #
-        #     if data is None:
-        #         print('데이터가 없습니다')
-        #
-        #     elif isinstance(data['itemList'], dict):
-        #         data_list = []
-        #         data_list.append(data['itemList'])
-        #
-        #         for arrive in range(len(data_list)):
-        #             arrive_dict = {}
-        #
-        #             arrive_dict['routeId'] = arrive_rtId
-        #             arrive_dict['routeNm'] = data_list[arrive]['rtNm']
-        #             arrive_dict['stnOrd'] = arrive_ord
-        #             arrive_dict['stnNm'] = data_list[arrive]['stNm']
-        #             arrive_dict['stnId'] = data_list[arrive]['stId']
-        #             arrive_dict['plainNo1'] = data_list[arrive]['plainNo1']
-        #             arrive_dict['arrmsg1'] = data_list[arrive]['arrmsg1']
-        #             arrive_dict['stnNm1'] = data_list[arrive]['stationNm1']
-        #             arrive_dict['plainNo2'] = data_list[arrive]['plainNo2']
-        #             arrive_dict['arrmsg2'] = data_list[arrive]['arrmsg2']
-        #             arrive_dict['stnNm2'] = data_list[arrive]['stationNm2']
-        #
-        #             arrive_list.append(arrive_dict)
-        #             print(f"노선ID : {arrive_rtId},"
-        #                   f" 노선이름 : {arrive_rtNm},"
-        #                   f" 노선순번 : {arrive_ord},"
-        #                   f" 정류소이름 : {arrive_stnNm},"
-        #                   f" 노선ID : {arrive_stnId},"
-        #                   f" 첫번째 도착예정버스 : {arrive_No1},"
-        #                   f" 현재 정류장 : {arrive_stnNm1},"
-        #                   f" 도착예정시간 : {arrive_msg1},"
-        #                   f" 두번째 도착예정버스 : {arrive_No2},"
-        #                   f" 도착예정시간 : {arrive_msg2},"
-        #                   f" 현재 정류장 : {arrive_stnNm2}")
+        url = f"http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRoute?" \
+              f"serviceKey={key}&stId={ddd['stnId']}&busRouteId={ddd['routeId']}&ord={ddd['ord']}"
+        content = requests.get(url).content
+        xmldict = xmltodict.parse(content)
+        data = xmldict['ServiceResult']['msgBody']
 
+        arrive_list = []
+        arrive_rtId = ddd['routeId']
+        arrive_ord = ddd['ord']
+
+        if data is None:
+            print('데이터가 없습니다')
+
+        elif isinstance(data['itemList'], dict):
+            data_list = []
+            data_list.append(data['itemList'])
+
+            for arrive in range(len(data_list)):
+                arrive_dict = {}
+
+                arrive_dict['routeId'] = arrive_rtId
+                arrive_dict['routeNm'] = data_list[arrive]['rtNm']
+                arrive_dict['stnOrd'] = arrive_ord
+                arrive_dict['stnNm'] = data_list[arrive]['stNm']
+                arrive_dict['stnId'] = data_list[arrive]['stId']
+                arrive_dict['plainNo1'] = data_list[arrive]['plainNo1']
+                arrive_dict['arrmsg1'] = data_list[arrive]['arrmsg1']
+                arrive_dict['stnNm1'] = data_list[arrive]['stationNm1']
+                arrive_dict['plainNo2'] = data_list[arrive]['plainNo2']
+                arrive_dict['arrmsg2'] = data_list[arrive]['arrmsg2']
+                arrive_dict['stnNm2'] = data_list[arrive]['stationNm2']
+
+                arrive_list.append(arrive_dict)
+                print(f"노선ID : {arrive_rtId},"
+                      f" 노선이름 : {arrive_dict['routeNm']},"
+                      f" 노선순번 : {arrive_ord},"
+                      f" 정류소이름 : {arrive_dict['stnNm']},"
+                      f" 정류소ID : {arrive_dict['stnId']},"
+                      f" 첫번째 도착예정버스 : {arrive_dict['plainNo1']},"
+                      f" 도착예정시간 : {arrive_dict['arrmsg1']},"
+                      f" 현재 정류장 : {arrive_dict['stnNm1']},"
+                      f" 두번째 도착예정버스 : {arrive_dict['plainNo2']},"
+                      f" 도착예정시간 : {arrive_dict['arrmsg2']},"
+                      f" 현재 정류장 : {arrive_dict['stnNm2']}")
     conn.close()
 
 
-get_some_data('arrive', 'stnNm', '봉천역')
+get_arrive_bus_info('arrive', 'stnNm', '을지로5가', 'stnId', 101000066)

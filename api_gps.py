e6ee3b9a2e701277742050879d79e05c2ee26322
Kim Hyeong Kyu <hyongkui@nate.com>
2023-01-04 21:06:50 +0900

    edit parameter

--- api_gps.py
+++ api_gps.py
@@ -3,16 +3,16 @@ from geopy.geocoders import Nominatim
 from api_station import get_stn_list
 
 # 현재위치
-def current_location():
-    here_req = requests.get("http://www.geoplugin.net/json.gp")
-
-    if (here_req.status_code != 200):
-        print("현재좌표를 불러올 수 없음")
-    else:
-        location = json.loads(here_req.text)
-        gps = {"gpsY": str(location["geoplugin_latitude"]), "gpsX": str(location["geoplugin_longitude"])}
-        print(gps['gpsY'], gps['gpsX'])
-    return gps
+# def current_location():
+#     here_req = requests.get("http://www.geoplugin.net/json.gp")
+#
+#     if (here_req.status_code != 200):
+#         print("현재좌표를 불러올 수 없음")
+#     else:
+#         location = json.loads(here_req.text)
+#         gps = {"gpsY": str(location["geoplugin_latitude"]), "gpsX": str(location["geoplugin_longitude"])}
+#         print(gps['gpsY'], gps['gpsX'])
+#     return gps
 
 
 
@@ -22,23 +22,22 @@ def geocoding(address):
     geolocoder = Nominatim(user_agent='South Korea')
     geo = geolocoder.geocode(address)
     gps = {"gpsY": str(geo.latitude), "gpsX": str(geo.longitude)}
-    print(gps)
 
     return gps
 
 
 
 # 특정 지역 좌표 입력해서 인근 정류소 구해보기
-def get_station_list(address):
+def get_station_list(address, rad):
     adr = geocoding(address)
-    gps = get_stn_list(adr['gpsX'], adr['gpsY'], 200)
+    gps = get_stn_list(adr['gpsX'], adr['gpsY'], rad)
     data_list = []
-    for i in range(len(gps)):
+    for data in range(len(gps)):
         data_dict = {}
-        data_dict['stnId'] = gps[i]['stnId']
-        data_dict['stnNm'] = gps[i]['stnNm']
-        data_dict['arsId'] = gps[i]['arsId']
+        data_dict['stnId'] = gps[data]['stnId']
+        data_dict['stnNm'] = gps[data]['stnNm']
+        data_dict['arsId'] = gps[data]['arsId']
         data_list.append(data_dict)
-    return gps
-get_station_list('을지로')
+
+get_station_list('을지로', 200)
 

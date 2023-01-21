7ebb11781e0694242e2c90823ae8833d0688181d
Kim Hyeong Kyu <hyongkui@nate.com>
2023-01-21 22:29:03 +0900

    서버구동&router연결

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
+from routers import get
 from fastapi import FastAPI
-from api.api_gps import get_station_list, get_cur_stn_list
-from api.api_bus_info import get_arrive_bus_info
 
 app = FastAPI()
 
-# 특정 지역 인근 정류소
-@app.get("/gps/{stn}")
-def read_gps(address: str, rad: int):
-    result = get_station_list(address, rad)
-    return result
-
-
-@app.get("/gps/{cur-stn}")
-def read_gps(rad: int):
-    result = get_cur_stn_list(rad)
-    return result
-
-
-# 정류소에 도착예정인 버스조회
-@app.get("/bus-info")
-def read_bus_info(stnNm: str, stnId: int):
-    result = get_arrive_bus_info(stnNm, stnId)
-    return result
-
+app.include_router(get.router)
 
 

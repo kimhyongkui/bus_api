214f8bd76fcf78863d004624f0c2ded506842319
Kim Hyeong Kyu <hyongkui@nate.com>
2023-01-27 23:51:55 +0900

    버스노선 검색

--- api/api_route.py
+++ api/api_route.py
@@ -6,16 +6,16 @@ import os
 load_dotenv()
 key = os.getenv('key')
 
-# 노선번호에 해당하는 노선 목록 조회
-url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey={key}"
 
-content = requests.get(url).content  # GET요청
-dict = xmltodict.parse(content)  # XML을 dictionary로 파싱
 
-data = dict['ServiceResult']['msgBody']['itemList']
 
 
 def get_route_all():
+    url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey={key}"
+    content = requests.get(url).content  # GET요청
+    dict = xmltodict.parse(content)  # XML을 dictionary로 파싱
+    data = dict['ServiceResult']['msgBody']['itemList']
+
     route_list = []
     for route in range(len(data)):
         route_dict = {}
@@ -24,3 +24,6 @@ def get_route_all():
         route_dict["routeId"] = data[route]["busRouteId"]
         route_list.append(route_dict)
     return route_list
+
+
+
--- routers/get.py
+++ routers/get.py
@@ -1,24 +1,34 @@
 from fastapi import APIRouter
 from api.api_stn_info import get_station_list, get_cur_stn_list
 from api.api_bus_info import get_arrive_bus_info
+from api.api_station import get_station
 
 router = APIRouter()
 
 # 특정 지역 인근 정류소
 @router.get("/gps/{stn}")
-def read_gps(address: str, rad: int):
+def get_gps(address: str, rad: int):
     result = get_station_list(address, rad)
     return result
 
 # 현재 위치 기반 인근 정류소
 @router.get("/gps/{cur-stn}")
-def read_gps(rad: int):
+
+def get_gps(rad: int):
     result = get_cur_stn_list(rad)
     return result
 
 
 # 정류소에 도착예정인 버스조회
 @router.get("/bus-info")
-def read_bus_info(stnNm: str, stnId: int):
+def get_bus_info(stnNm: str, stnId: int):
     result = get_arrive_bus_info(stnNm, stnId)
+    return result
+
+# 버스 노선 검색
+@router.get("/route-info")
+def get_route_info(routeId):
+    # 버스 노선을 입력하면 해당 노선의 리스트를 띄워줌
+
+    result = get_station(routeId)
     return result

960104dcbfac76a34b2c5519bc04057455cf215a
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-15 23:48:11 +0900

    예외처리 테스트

--- routers/get/arrive_bus.py
+++ routers/get/arrive_bus.py
@@ -7,5 +7,8 @@ router = APIRouter(prefix="/bus-api")
 # 정류소에 도착예정인 버스조회
 @router.get('/arrive-bus-info/{stnNm}', tags=["도착 예정 버스 조회"])
 def arrive_bus_info(stnNm, stnId):
-    result = get_arrive_bus_info(stnNm, stnId)
-    return result
+    try:
+        result = get_arrive_bus_info(stnNm, stnId)
+        return result
+    except:
+        return '정류소 이름과 ID를 확인하세요'
--- routers/get/bus_location.py
+++ routers/get/bus_location.py
@@ -7,6 +7,9 @@ router = APIRouter(prefix="/bus-api")
 # 특정 노선 실시간 버스 위치
 @router.get('/bus-location/{routeNm}', tags=["실시간 버스 위치 조회"])
 def realtime_bus_loc(routeNm):
-    if get_bus_info(routeNm):
+    try:
         result = get_bus_info(routeNm)
         return result
+    except:
+        return '노선명을 다시 확인하세요'
+print(realtime_bus_loc()

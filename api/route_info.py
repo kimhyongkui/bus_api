36d04a957e6ffc826f878bc8e6d2e8b20e1543ba
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-18 21:45:55 +0900

    get_route_info_all함수를 get_route_info함수를 이용해서 간소화

--- api/route_info.py
+++ api/route_info.py
@@ -28,8 +28,7 @@ def get_route_info(routeid):
         return arrive_list
 
     except TypeError as err:
-        return f"{err}, '노선ID를 확인하세요'"
-
+        return f"{err}, 노선ID를 확인하세요"
 
 # 모든 경유노선의 전체정류소 데이터 얻기
 def get_route_info_all():
@@ -37,21 +36,8 @@ def get_route_info_all():
         route_list = get_route_all()
         arrive_list = []
         for route in route_list:
-            url = f"http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?" \
-                  f"serviceKey={os.getenv('key')}&busRouteId={route['routeId']}"
-            content = requests.get(url).content
-            dict = xmltodict.parse(content)
-            data = dict['ServiceResult']['msgBody']['itemList']
-            for arrive in range(len(data)):
-                arrive_dict = {
-                    'routeId': route['routeId'],
-                    'routeNm': data[arrive]['rtNm'],
-                    'stnOrd': data[arrive]['staOrd'],
-                    'stnNm': data[arrive]['stNm'],
-                    'stnId': data[arrive]['stId']
-                }
-                arrive_list.append(arrive_dict)
+            arrive_list.append(get_route_info(route['routeId']))
         return arrive_list
 
     except TypeError as err:
-        return f"{err}, '오류가 난 노선ID를 확인하세요'"
+        return f"{err}, 오류가 난 노선ID를 확인하세요"

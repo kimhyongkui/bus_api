d5c894c3d2d56f5529103658f2aed4c9e8922e49
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-19 23:49:36 +0900

    connection pool 적용과 예외처리

--- db/get/db_data.py
+++ db/get/db_data.py
@@ -2,32 +2,66 @@ from db.connection import conn
 
 
 def get_stn_data(stnNm, stnId):
-    curs = conn.cursor()
-    sql = f"SELECT * FROM route_info WHERE stnNm='{stnNm}' AND stnId={stnId}"
-    curs.execute(sql)
-    result = curs.fetchall()
+    try:
+        conn.init()
+        connection = conn.get_conn()
+        curs = connection.cursor()
+        sql = f"SELECT * FROM route_info WHERE stnNm='{stnNm}' AND stnId={stnId}"
+        curs.execute(sql)
+        result = curs.fetchall()
+        conn.release(connection)
+        if not result:
+            result = '다시 검색하세요'
+    except Exception as err:
+        result = f"{err}, 다시 검색하세요"
     return result
 
 
 def get_route_list(routeNm):
-    curs = conn.cursor()
-    sql = f"SELECT routeNm, stnOrd, stnNm FROM route_info WHERE routeNm = '{routeNm}'"
-    curs.execute(sql)
-    result = curs.fetchall()
+    try:
+        conn.init()
+        connection = conn.get_conn()
+        curs = connection.cursor()
+        sql = f"SELECT routeNm, stnOrd, stnNm FROM route_info WHERE routeNm = '{routeNm}'"
+        curs.execute(sql)
+        result = curs.fetchall()
+        conn.release(connection)
+        if not result:
+            result = '다시 검색하세요'
+    except Exception as err:
+        result = f"{err}, 다시 검색하세요"
     return result
 
 
 def get_route_name(routeNm):
-    curs = conn.cursor()
-    sql = f"SELECT * FROM route WHERE routeNm = '{routeNm}'"
-    curs.execute(sql)
-    result = curs.fetchone()
-    return result[3]
+    try:
+        conn.init()
+        connection = conn.get_conn()
+        curs = connection.cursor()
+        sql = f"SELECT * FROM route WHERE routeNm = '{routeNm}'"
+        curs.execute(sql)
+        result = curs.fetchone()
+        conn.release(connection)
+        if result:
+            result = result[3]
+        else:
+            result = '다시 검색하세요'
+    except Exception as err:
+        result = f"{err}, 다시 검색하세요"
+    return result
 
 
 def get_stn_name(stnNm):
-    curs = conn.cursor()
-    sql = f"SELECT stnNm, stnId, arsId, direction FROM station WHERE stnNm = '{stnNm}' GROUP BY stnId"
-    curs.execute(sql)
-    result = curs.fetchall()
+    try:
+        conn.init()
+        connection = conn.get_conn()
+        curs = connection.cursor()
+        sql = f"SELECT stnNm, stnId, arsId, direction FROM station WHERE stnNm = '{stnNm}' GROUP BY stnId"
+        curs.execute(sql)
+        result = curs.fetchall()
+        conn.release(connection)
+        if not result:
+            result = '다시 검색하세요'
+    except Exception as err:
+        result = f"{err}, 다시 검색하세요"
     return result

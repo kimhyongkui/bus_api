fdb12fe1ca8035c3e50ba315e58fb634db9aa110
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-24 00:10:58 +0900

    라우터처리나 함수호출시에 문제가 있어서 함수를 나누는 쪽으로 결정

--- api/stn_info.py
+++ api/stn_info.py
@@ -26,6 +26,7 @@ def get_station_list(address, rad):
     except Exception as err:
         return f"{err}, 에러 발생"
 
+
 # 현재 좌표 입력해서 인근 정류소 구해보기
 def get_cur_stn_list(rad):
     try:
@@ -49,31 +50,3 @@ def get_cur_stn_list(rad):
 
     except Exception as err:
         return f"{err}, 에러 발생"
-
-
-# def stn_list(address=None, *, rad):
-#     if address:
-#         adr = specific_location(address)
-#     else:
-#         adr = current_location()
-#     try:
-#         gps = get_stn_list(adr['gpsX'], adr['gpsY'], rad)
-#         data_list = []
-#
-#         if gps == 'Null':
-#             data_list = '주변에 정류소가 없습니다'
-#
-#         else:
-#             for data in gps:
-#                 data_dict = {
-#                     'stnId': data['stnId'],
-#                     'stnNm': data['stnNm'],
-#                     'arsId': data['arsId'],
-#                     'dist': data['dist']
-#                 }
-#                 data_list.append(data_dict)
-#
-#         return data_list
-#
-#     except Exception as err:
-#         return f"{err}, 에러 발생"

b55360ca3820b01353909723e41353467f00593d
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-24 00:00:37 +0900

    if문의 순서와 내용 수정

--- api/search_station.py
+++ api/search_station.py
@@ -8,37 +8,38 @@ load_dotenv()
 
 # 특정 좌표 인근 정류소 데이터 얻기
 def get_stn_list(gpsx, gpsy, radius):
-    stn_list = []
-    if gpsx == 'null':
-        stn_list = 'Null'
-    else:
+    try:
+        stn_list = []
         url = f"http://ws.bus.go.kr/api/rest/stationinfo/getStationByPos?" \
               f"serviceKey={os.getenv('key')}&tmX={gpsx}&tmY={gpsy}&radius={radius}"
         content = requests.get(url).content
         xmldict = xmltodict.parse(content)
-        data = xmldict['ServiceResult']['msgBody']
-        if data is None:
-            stn_list = 'Null'
-        elif isinstance(data['itemList'], dict):
+        data = xmldict['ServiceResult']['msgBody']['itemList']
+        if isinstance(data, dict):
             stn_dict = {
-                'stnId': data['itemList']['stationId'],
-                'stnNm': data['itemList']['stationNm'],
-                'arsId': data['itemList']['arsId'],
-                'gpsX': data['itemList']['gpsX'],
-                'gpsY': data['itemList']['gpsY'],
-                'dist': data['itemList']['dist']
+                'stnId': data['stationId'],
+                'stnNm': data['stationNm'],
+                'arsId': data['arsId'],
+                'gpsX': data['gpsX'],
+                'gpsY': data['gpsY'],
+                'dist': data['dist']
             }
             stn_list.append(stn_dict)
         else:
-            data_list = data['itemList']
-            for station in range(len(data_list)):
+            for station in data:
                 stn_dict = {
-                    'stnId': data_list[station]['stationId'],
-                    'stnNm': data_list[station]['stationNm'],
-                    'arsId': data_list[station]['arsId'],
-                    'gpsX': data_list[station]['gpsX'],
-                    'gpsY': data_list[station]['gpsY'],
-                    'dist': data_list[station]['dist']
+                    'stnId': station['stationId'],
+                    'stnNm': station['stationNm'],
+                    'arsId': station['arsId'],
+                    'gpsX': station['gpsX'],
+                    'gpsY': station['gpsY'],
+                    'dist': station['dist']
                 }
                 stn_list.append(stn_dict)
-    return stn_list
+
+        return stn_list
+    # 매개변수 값이 잘못되었거나 data의 값이 없을때
+    except TypeError:
+        return 'Null'
+    except Exception as err:
+        return err

456eec92a4e76eb1d0b627d65b2d22b283d6412f
Kim Hyeong Kyu <hyongkui@nate.com>
2023-02-26 21:32:41 +0900

    SqlAlchemy로 전환

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-from pymysqlpool.pool import Pool
 import os
 
 load_dotenv()
 
-conn = Pool(host='127.0.0.1',
-            user='root',
-            password=os.getenv('user_pwd'),
-            db='prac',
-            charset='utf8'
-            )
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqldb://root:{os.getenv('user_pwd')}@127.0.0.1/prac"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()

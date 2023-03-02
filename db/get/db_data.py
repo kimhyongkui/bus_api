0278430cbbec7594c264aa7f631a01a232945475
Kim Hyeong Kyu <hyongkui@nate.com>
2023-03-02 23:47:51 +0900

    filter(), filter_by() 메소드

--- db/get/db_data.py
+++ db/get/db_data.py
@@ -9,7 +9,7 @@ session = Session()
 def get_stn_data(stn_name, stn_id):
     try:
         result = session.query(route_data). \
-            filter(route_data.stnNm == f"{stn_name}", route_data.stnId == f"{stn_id}").all()
+            filter_by(stnNm=f"{stn_name}", stnId=f"{stn_id}").all()
         if not result:
             result = '데이터가 없습니다. 다시 검색하세요'
         else:
@@ -34,8 +34,6 @@ def get_stn_data(stn_name, stn_id):
     return result
 
 
-# print(get_stn_data('안암역', 107000205))
-
 def get_route_data(route_name):
     try:
         result = session.query(route_data).filter(route_data.routeNm.like(f"%{route_name}%")).all()
@@ -53,20 +51,18 @@ def get_route_data(route_name):
 
 def get_route_list(route_name):
     try:
-        result = session.query(route_list).filter(route_list.routeNm.like(f"%{route_name}%")).first()
+        result = session.query(route_list).filter_by(routeNm=route_name).first()
         if not result:
             result = '데이터가 없습니다. 다시 검색하세요'
 
         else:
-            data_list = []
-            for data in result:
-                data_dict = {
-                    'routeNm': data.routeNm,
-                    'routeAbrv': data.routeAbrv,
-                    'routeId': data.routeId
-                }
-                data_list.append(data_dict)
-            result = data_list
+            result_dict = {
+                'routeNm': result.routeNm,
+                'routeAbrv': result.routeAbrv,
+                'routeId': result.routeId
+            }
+
+            result = result_dict
 
     except Exception as err:
         result = f"{err}, 노선 이름을 확인하세요"
@@ -75,9 +71,6 @@ def get_route_list(route_name):
         session.close()
 
     return result
-
-
-print(get_route_list(6001))
 
 
 def get_stn_name(stn_name):

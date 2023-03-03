1251ec0cc77883cef0c4c72a28148f6e7718357b
Kim Hyeong Kyu <hyongkui@nate.com>
2023-03-03 23:55:41 +0900

    test sqlalchemy post

--- db/post/route_list.py
+++ db/post/route_list.py
@@ -1,26 +1,36 @@
 from api.route import get_all_route_list
+from sqlalchemy.orm import sessionmaker
+from db.connection import engine
+from db.models import test_table
 from dotenv import load_dotenv
-from db.connection import conn
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 load_dotenv()
 
 
 # 각 노선의 아이디와 이름 DB저장
 def add_route_list():
-    conn.init()
-    connection = conn.get_conn()
-    curs = connection.cursor()
+
     try:
-        route_list = get_all_route_list()
-        for route_data in route_list:
-            sql = f"INSERT INTO route_list (routeNm, routeAbrv, routeId) " \
-                  f"VALUES ('{route_data['routeNm']}', '{route_data['routeAbrv']}', {route_data['routeId']})"
-            curs.execute(sql)
-        connection.commit()
+        all_route_list = get_all_route_list()
+
+        for route_data in all_route_list:
+            route = test_table(
+                no=route_data['no'],
+                routeNm=route_data['routeNm'],
+                routeAbrv=route_data['routeAbrv'],
+                routeId=route_data['routeId']
+            )
+            session.add(route)
+        session.commit()
         print('DB 저장 완료')
 
     except Exception as err:
         return f"{err}"
 
     finally:
-        conn.release(connection)
+        session.close()
+
+print(add_route_list())

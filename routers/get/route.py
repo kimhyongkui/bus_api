1e77e71396ad259f02d902cb6f217dc1deeb0a5f
Kim Hyeong Kyu <hyongkui@nate.com>
2023-03-21 23:58:46 +0900

    테스트

--- routers/get/route.py
+++ routers/get/route.py
@@ -10,6 +10,5 @@ def route_data(route_name: str):
     try:
         result = get_route_data(route_name)
         return result
-
     except Exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="에러 발생")

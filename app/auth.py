f6ddf9e2262be5523fa15c4aaf8e172bb4b9c0c7
Kim Hyeong Kyu <hyongkui@nate.com>
2023-05-03 23:57:16 +0900

    비밀번호 해쉬기능

---
+++ app/auth.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
+
+def password_hash(pwd):
+    return bcrypt_context.hash(pwd)
+
+
+def verify_password(pwd, hashed_pwd):
+    return bcrypt_context.verify(pwd, hashed_pwd)

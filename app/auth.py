269dc44e06ef4d33f363dfa025a3a9f2c7c840b5
Kim Hyeong Kyu <hyongkui@nate.com>
2023-05-05 17:50:49 +0900

    비밀번호 함수 이동, 토큰만료일 체크, 종속성 부여함수 생성

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,28 @@
-from passlib.context import CryptContext
+from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from db.post.login import SECRET_KEY, ALGORITHM
+from datetime import datetime
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/bus-api/login")
 
 
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        timestamp = datetime.utcfromtimestamp(payload["exp"])
+        if timestamp < datetime.utcnow():
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="만료된 토큰")
+        return payload
+
+    except jwt.JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="자격이 검증되지 않았습니다.")
+
+    except Exception as err:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
 
 
-def password_hash(pwd):
-    return bcrypt_context.hash(pwd)
-
-
-def verify_password(pwd, hashed_pwd):
-    return bcrypt_context.verify(pwd, hashed_pwd)
+def get_admin(current_user: dict = Depends(get_current_user)):
+    if current_user["user_id"] != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="권한이 없습니다")
+    return current_user

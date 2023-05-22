d06b0dad75bbd866099bc4ab13ecbf7958724512
Kim Hyeong Kyu <hyongkui@nate.com>
2023-05-22 23:59:58 +0900

    앤드포인트 수정

--- routers/post/vehicle.py
+++ routers/post/vehicle.py
@@ -5,13 +5,13 @@ from app.auth import get_admin
 router = APIRouter(prefix="/vehicle")
 
 
-@router.post('/route-data', tags=["버스"], dependencies=[Depends(get_admin)])
+@router.post('/vehicle-data', tags=["버스"], dependencies=[Depends(get_admin)])
 def vehicle_data(route_name):
     result = add_veh_data(route_name)
     return result
 
 
-@router.post('/route-data/all', tags=["버스"], dependencies=[Depends(get_admin)])
+@router.post('/vehicle-data/all', tags=["버스"], dependencies=[Depends(get_admin)])
 def all_vehicle_data():
     result = add_all_veh_data()
     return result

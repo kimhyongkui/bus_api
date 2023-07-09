07a3255d2cb0e89e37b06a5e7cb793e67070a3d4
Kim Hyeong Kyu <hyongkui@nate.com>
2023-07-10 06:31:13 +0900

    Redis 다시 한번 시도

--- app/auth.py
+++ app/auth.py
@@ -3,6 +3,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from tokens.create_token import SECRET_KEY, ALGORITHM
 from datetime import datetime
+from db.post.login import redis_client
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/bus-api/login")
 
@@ -13,7 +14,16 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         timestamp = datetime.utcfromtimestamp(payload["exp"])
         if timestamp < datetime.utcnow():
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="만료된 토큰")
-        return payload
+
+        user_id = redis_client.get(token)
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="유효하지 않은 토큰")
+        user_id = user_id.decode()
+
+        return {
+            "user_id": user_id,
+            "token": token
+        }
 
     except jwt.JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="자격이 검증되지 않았습니다.")
@@ -25,5 +35,4 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 def get_admin(current_user: dict = Depends(get_current_user)):
     if current_user["user_id"] != "admin":
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="권한이 없습니다")
-
     return current_user
--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import uvicorn
 
 load_dotenv()
 
-app = FastAPI(title="bus_api")
+app = FastAPI(title="서울시 버스 정보 조회")
 
+# get
 app.include_router(station.router, prefix="/bus-api")
 app.include_router(arrive_bus.router, prefix="/bus-api")
 app.include_router(route.router, prefix="/bus-api")
 app.include_router(bus_location.router, prefix="/bus-api")
-
+# post
 app.include_router(route_data.router, prefix="/bus-api")
 app.include_router(route_list.router, prefix="/bus-api")
 app.include_router(station_data.router, prefix="/bus-api")
@@ -25,11 +26,11 @@ app.include_router(login.router, prefix="/bus-api")
 def main():
     environment = os.getenv("ENVIRONMENT")
     if environment == "local":
-        # 로컬 환경
+        # 로컬 환경 설정
         host = "localhost"
         port = 8000
     else:
-        # 배포 환경
+        # 배포 환경 설정
         host = os.getenv("SERVER_HOST")
         port = int(os.getenv("SERVER_PORT"))
 
